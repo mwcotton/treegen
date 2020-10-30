25e13e9b28652fae324342641b43c713e1832725
Alex Norman <alex96norman@gmail.com>
2020-10-30 11:26:03 +0000

    Add pages to path in nuxt.config.js for GitHub pages deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
 
   // Add the router base
   router: {
-    base: '/Down-To-Programme/treegen/'
+    base: '/Down-To-Programme/treegen/pages/'
   },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
