742e5ae3353310871613cd502d3925d78d1e8e0d
Emma Borsier <emmaborsier@gmail.com>
2020-10-30 20:15:28 +0000

    Add content to about and links pages

--- pages/links.vue
+++ pages/links.vue
@@ -1,7 +1,35 @@
 <template>
   <v-layout>
-    <v-flex class="text-center">
-      <h1>This is the links page</h1>
+    <v-flex class="text-left">
+      <h1>Links</h1>
+      <ul>
+        <li><a href="http://algorithmicbotany.org/papers/#abop">Here</a> is a link to the textbook that 
+        inspired this project, An Algorithmic Beauty of Plants. You can learn more about L-systems in the 
+        first chapter.</li>
+        <li><a href="https://github.com/Down-To-Programme/treegen">Here</a> is a link to our GitHub 
+        repository for this project, if you would like to report a bug, provide 
+        feedback or make take a look at our code. There, you can find more info about our 
+        development process in the README.</li>
+        <li><a href="https://eng.qualia.com/drawing-fractals-in-the-browser-with-l-systems-and-es6-6cecfd74e084">
+        Here</a> is a link to a useful tutorial that you can follow to learn how to develop your own L-system generation software.</li>
+      </ul>
     </v-flex>
   </v-layout>
 </template>
+
+<style scoped>
+h1 .special-color {
+  color: greenyellow;
+}
+
+p {
+    color: rgb(180, 180, 180);  
+}
+li {
+    color: rgb(180, 180, 180);  
+}
+h3 {
+    color: greenyellow;
+}
+
+</style>
