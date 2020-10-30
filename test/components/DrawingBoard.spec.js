67c767fed5b278462d5541432208b1cf2b806d97
DSeiferth <david.seiferth@dtc.ox.ac.uk>
2020-10-30 15:10:24 +0000

    #33 test for props in DrawingBoard

--- test/components/DrawingBoard.spec.js
+++ test/components/DrawingBoard.spec.js
@@ -8,3 +8,24 @@ test('Drawingboard.', (t) => {
   // t.true(wrapper.contains('.vue-p5'));
   t.true(1 == 1);
 });
+
+test('Test props', (t) => {
+  const wrapper = shallow(DrawingBoard, {
+    propsData: {
+      branchLen: 2,
+      iters: 3,
+      branchAng: 20,
+      initState: 'X',
+      choiceRule: 'Deterministic',
+      fRule: 'FF',
+      xRule: 'F[+X]F[-X]+X',
+    },
+  });
+  t.is(wrapper.props().branchLen, 2);
+  t.is(wrapper.props().iters, 3);
+  t.is(wrapper.props().branchAng, 20);
+  t.is(wrapper.props().initState, 'X');
+  t.is(wrapper.props().choiceRule, 'Deterministic');
+  t.is(wrapper.props().fRule, 'FF');
+  t.is(wrapper.props().xRule, 'F[+X]F[-X]+X');
+});
